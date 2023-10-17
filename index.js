6c6fdefed81c45512786df5af50af4d9a4d7a986
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-10-17 21:57:43 +0600

    Initial Project Setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Crud is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Simple Crud is Running on port ${port}`);
+});
